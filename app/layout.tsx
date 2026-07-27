bcdfdce0cce876f946c92a7bf573ac1320fb0072
fardeenpathan <md.fardeen5236@gmail.com>
2026-07-27 17:41:07 +0530

    update code

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
   metadataBase: new URL("https://aura-demo-flame.vercel.app/"),
 
   openGraph: {
-    title: "Harpreet & Ritika",
-    description: "Join as they begin their forever. 8, 9 & 11 March 2026",
+    title: "Varun Prasad & Yuvashri",
+    description: "Join as they begin their forever. 30th August 2026",
     url: "https://aura-demo-flame.vercel.app/",
     siteName: "InviteArc",
     images: [
@@ -27,7 +27,7 @@ export const metadata: Metadata = {
         url: "/og.jpg",
         width: 1200,
         height: 630,
-        alt: "Harpreet & Ritika ",
+        alt: "Varun Prasad & Yuvashri ",
       }, 
     ],
     type: "website",
@@ -36,8 +36,8 @@ export const metadata: Metadata = {
 
   twitter: {
     card: "summary_large_image",
-    title: "Harpreet & Ritika",
-    description: "Join as they begin their forever. 8, 9 & 11 March, 2026",
+    title: "Varun Prasad & Yuvashri",
+    description: "Join as they begin their forever. 30th August 2026",
     images: ["/og.jpg"],
   },
 
